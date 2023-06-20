2a2672bd5285fa0a53dbbc3d330bc2b3e268f701
UselessManS90 <132806181+UselessManS90@users.noreply.github.com>
2023-06-20 19:58:46 +0800

    DieScript.lua

---
+++ DieScript.lua
@@ -0,0 +1,25 @@
+--[[Credits to ManHoppin
+]]--
+
+wait(0.1)
+    game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(varr, "All")
+end
+
+local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))()
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
+
+Notification:Notify(
+    {Title = "Rebel-Adminus", Description = "Rebel-Adminus Loaded!"},
+    {OutlineColor = Color3.fromRGB(0,0,255),Time = 5, Type = "Option"},
+    {Image = "http://www.roblox.com/asset/?id=8564501873", ImageColor = Color3.fromRGB(255, 84, 84), Callback = function(State) 
+
+local player = game.Players.LocalPlayer
+
+function ResetPlayer()
+  player:LoadCharacter()
+end
+
+ResetPlayer()
+
+--[[ Die Script
+]]--
